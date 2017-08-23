44724a69f1d6038d0b478d106393bd79db3083f4
IonescuA <alina.ionescu@haufe.-lexware.com>
2017-08-23 16:07:54 +0300

    added scss and nav search support

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,25 +1,16 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-/*var Nav = React.createClass({
-    render: function () {
-        return (
-            <div>
-                <h2>Nav component</h2>
-                <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
-                <Link to="/about" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
-                <Link to="/examples" activeClassName="active"activeStyle={{fontWeight:'bold'}}>Examples</Link>
-
-                <a href="#/about">Go To About</a>
-            </div>
-        );
-    }
-});*/
-
 var Nav = React.createClass({
     onSearch: function (e) {
         e.preventDefault();
-        alert('Not yet wired up!')
+        var location = this.refs.searchField.value;
+        var encodedLocation = encodeURIComponent(location);
+
+        if(location.length > 0) {
+            this.refs.searchField.value='';
+            window.location.hash = '#/?location=' + encodedLocation;
+        }
     },
     render: function () {
         return (
@@ -42,7 +33,7 @@ var Nav = React.createClass({
                     <form onSubmit={this.onSearch}>
                         <ul className="menu">
                             <li>
-                                <input type="search" placeholder="Search weather by city"/>
+                                <input type="search" ref="searchField" placeholder="Search weather by city"/>
                             </li>
                             <li>
                                 <input type="submit" className="button" value="get weather"/>
